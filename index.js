259c92cfc7673ba960fde00abe74c2f0a299a042
Suraiya Hameed <v-suhame@microsoft.com>
2017-10-31 15:28:00 -0700

    fix: move handshake code out of tedious package into node-kerberos

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// Get the Kerberos library
-module.exports = require('./lib/kerberos');
-// Set up the auth processes
+const KerberosAuthProvider = require('./lib/kerberosAuthProvider.js').KerberosAuthProvider;
+
 module.exports['processes'] = {
   MongoAuthProcess: require('./lib/auth_processes/mongodb').MongoAuthProcess
-}
+};
+
+module.exports = function(options) {
+  return function(connection) {
+    console.log('new KerberosAuthProvider ');
+    return new KerberosAuthProvider(connection, options);
+  };
+};
